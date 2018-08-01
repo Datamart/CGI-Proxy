64fd1daeee6a4fee10961bce2944d22ed18f3075
Valentin Podkamennyi <valentin@dtm.io>
2018-07-31 21:43:06 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cgi-proxy',
+    version='10.26',
+    description='The simple HTTP proxy.',
+    url='https://github.com/Datamart/CGI-Proxy',
+    author='Valentin Podkamennyi, Alex Krailo',
+    author_email='Valentin Podkamennyi <valentin@dtm.io>, Alex Krailo <alex@dtm.io>',
+    license='Apache 2.0',
+    scripts=['proxy']
+)
